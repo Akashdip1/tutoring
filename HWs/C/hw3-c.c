48aa7698bcd71db059b06386b539f442a351dced
chanchal3728 <165544238+chanchal3728@users.noreply.github.com>
2024-04-13 11:36:52 +0530

    submited hw3

---
+++ HWs/C/hw3-c.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+    float celcius;
+    float fahrenheit;
+    printf("enter tempreture in celcius :");
+    fahrenheit=(celcius * 1.8) + 32;
+   printf("celcius = %f & fahrenheit = %f",celcius,fahrenheit);
+}
