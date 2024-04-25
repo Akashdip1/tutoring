60ee626e8c032829584b69a5ca3ee578b7dcbbc6
Akashdip1 <akashdip.baral@gmail.com>
2024-04-25 19:10:41 +0530

    Class 11 slides, updated examples

--- Examples/average.c
+++ Examples/average.c
@@ -1,12 +1,28 @@
 #include <cs50.h>
 #include <stdio.h>
 
+float average(int array[]);
+
 int main(void){
-    int scores[3];
+    int scores[4]; 
     
-    for (int i = 0; i < 3; i++){
+    for (int i = 0; i < 4; i++){
         scores[i] = get_int("Score%i: ", i+1);
     }
+    float av_score = average(scores);
+    printf("Average: %f", av_score);
 
-    printf("Score3 : %i\n",scores[3] );
+}
+
+// average --> float 
+float average(int array[])
+{
+    int sum = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        sum = sum + array[i];      // i = 0, sum = 0 + 72; i = 1, sum = 72 + 73 ; i = 2, sum = 145 + 33
+    }
+    float av = sum/4.0;
+
+    return av;
 }
