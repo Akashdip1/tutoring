77dbb3a2257639005f27c043615df3bdac2ba412
Piyushpradhan143 <165451915+Piyushpradhan143@users.noreply.github.com>
2024-06-13 17:54:12 +0530

    Create 1.c

---
+++ HWs/K/hackerrank/1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+int main()
+{
+  string world = get_string("tell something abput the world :");
+  printf("hello world :\n %s \n",world);
+}
