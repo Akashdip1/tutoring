60ea3ee52bf9061fcbb067e1226952cbc10764f2
Akashdip1 <akashdip.baral@gmail.com>
2024-06-20 18:55:36 +0530

    pointers starting

---
+++ Examples/commandline.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Improper Input\n");
+    }
+    else {
+        printf("Proper Input\n");
+    }
+}
