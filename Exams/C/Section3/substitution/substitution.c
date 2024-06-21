71198dd8ecbfa31e57fa05fca503505ec5ba7c78
chanchal3728 <165544238+chanchal3728@users.noreply.github.com>
2024-06-21 10:59:17 +0530

    Update substitution.c-txt

---
+++ Exams/C/Section3/substitution/substitution.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char ch[100];
+    printf("word =");
+    scanf("%s",ch);
+
+    int l = strlen(ch);
+
+    printf("original word = %s \n",ch);
+
+    for(int i = 0;i < l;i++)
+    {
+        if(ch[i] >= 65 && ch[i] <= 90)
+        ch[i] = ch[i] + 3;
+        else if(ch[i] >= 97 && ch[i] <=122)
+        ch[i] = ch[i] + 4;
+    }
+    printf("key word = %s \n",ch);
+
+    return 0;
+}
