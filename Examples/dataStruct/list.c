ddc352d3400c24169deef54e5d4f359f91a43ac8
Akashdip1 <akashdip.baral@gmail.com>
2024-07-06 17:22:41 +0530

    Data Structures - 3

--- Examples/dataStruct/list.c
+++ Examples/dataStruct/list.c
@@ -1,56 +1,86 @@
+// Implements a sorted list of numbers using a linked list
+
+#include <cs50.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-
-// Defining node data structure
 typedef struct node
 {
     int number;
     struct node *next;
-}node;
+}
+node;
 
 int main(int argc, char *argv[])
 {
-    //create a list pointer who will point to the first node
-    node *list;
-    list = NULL;
-    
-    //add command line argument numbers to list
+    // Memory for numbers
+    node *list = NULL;
+
+    // For each command-line argument
     for (int i = 1; i < argc; i++)
     {
-        // Convert string to int
-        int number = atoi(argv[i]);   // string 1 2 3 -> int 1 2 3
+        // Convert argument to int
+        int number = atoi(argv[i]);
 
-        // Allocate a new node with a temp pointer
+        // Allocate node for number
         node *n = malloc(sizeof(node));
-        
+        if (n == NULL)
+        {
+            return 1;
+        }
         n->number = number;
         n->next = NULL;
 
-        // Prepend node to the list
-        n->next = list;
-        list = n;
+        // If list is empty
+        if (list == NULL)
+        {
+            list = n;
+        }
+
+        // If number belongs at beginning of list
+        else if (n->number < list->number)
+        {
+            n->next = list;
+            list = n; 
+        }
+
+        // If number belongs later in list
+        else
+        {
+            // Iterate over nodes in list
+            for (node *ptr = list; ptr != NULL; ptr = ptr->next)
+            {
+                // If at end of list
+                if (ptr->next == NULL)
+                {
+                    // Append node
+                    ptr->next = n;
+                    break;
+                }
+
+                // If in middle of list
+                if (n->number < ptr->next->number)
+                {
+                    n->next = ptr->next;
+                    ptr->next = n;
+                    break;
+                }
+            }
+        }
     }
 
-    //Print numbers
-    node *ptr = list;
-
-    //Follow the pointers
-    while(ptr != NULL)
+    // Print numbers
+    for (node *ptr = list; ptr != NULL; ptr = ptr->next)
     {
-        printf("%i\n",ptr->number);
-        ptr = ptr->next;
+        printf("%i\n", ptr->number);
     }
 
     // Free memory
-    ptr = list;
-    while(ptr != NULL)
+    node *ptr = list;
+    while (ptr != NULL)
     {
         node *next = ptr->next;
         free(ptr);
         ptr = next;
-
     }
-
 }
-
