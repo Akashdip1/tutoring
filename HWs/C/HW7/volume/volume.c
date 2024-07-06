a642ecc0ce1b7b89ccea6806d280aa00902b1e46
chanchal3728 <165544238+chanchal3728@users.noreply.github.com>
2024-07-06 11:35:49 +0530

    hws-7.txt.c

--- HWs/C/HW7/volume/volume.c
+++ HWs/C/HW7/volume/volume.c
@@ -34,10 +34,22 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
+    uint8_t header[HEADER_SIZE];
+    fread(header, sizeof(uint8_t), HEADER_SIZE, input);
+    fwrite(header, sizeof(uint8_t), HEADER_SIZE, output);
 
     // TODO: Read samples from input file and write updated data to output file
+    int16_t sample;
+    while (fread(&sample, sizeof(int16_t), 1, input))
+    {
+        sample *= factor;
+        
+        fwrite(&sample, sizeof(int16_t), 1, output);
+    }
 
     // Close files
     fclose(input);
     fclose(output);
+
+    return 0;
 }
